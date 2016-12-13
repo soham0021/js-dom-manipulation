a1c1f51bb38e6d9d42b29a4c31678dc6225a43bb
soham patel <sohampatel9055@yahoo.com>
2016-12-13 18:46:39 -0500

    adds initial docs for websites

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// creat a variable with string stored inside
+ var name = "soham"
+// creat a variable with number stored inside
+var number = 13;
+// creat a variable with boolean stored inside
+var iam= true;
+// creat a variable with an array stored inside
+var states = ["nj",'pa','de'];
+for (var i =0; i < states.length; i++) {
+	console.log(states[i]);
+}
+/* creat a for loop that loops over your array 
+	logging each iteam to the console
+*/
